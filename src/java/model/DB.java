7dd9e489576cd881a24e02d5952d3423f3e9c0e3
KRM <KRM@KRM-PC>
2020-08-12 00:21:16 +0200

    get Community service Council

--- src/java/model/DB.java
+++ src/java/model/DB.java
@@ -477,24 +477,28 @@ public class DB {
                 item.setCsc_addedBy(rs.getInt("csc_addedBy"));
                 item.setName(rs.getString("name"));
                 item.setPosition(rs.getString("position"));
-                InputStream inputStream = ((rs.getBlob("image").getBinaryStream()));
+                try {
+                    InputStream inputStream = ((rs.getBlob("image").getBinaryStream()));
 
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                byte[] buffer = new byte[4096];
-                int bytesRead = -1;
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = -1;
 
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+
+                    byte[] imageBytes = outputStream.toByteArray();
+                    String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+
+                    inputStream.close();
+                    outputStream.close();
+
+                    item.setBase64_image(base64Image);
+                } catch (Exception e) {
+
+                    item.setBase64_image("noImage");
                 }
-
-                byte[] imageBytes = outputStream.toByteArray();
-                String base64Image = Base64.getEncoder().encodeToString(imageBytes);
-
-                inputStream.close();
-                outputStream.close();
-
-                item.setBase64_image(base64Image);
-
                 data.add(item);
             }
 
@@ -502,11 +506,7 @@ public class DB {
         } catch (SQLException ex) {
             ex.printStackTrace();
             return null;
-
-        } catch (IOException ex) {
-            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
 
     }
 
--- web/getCommunity_service_council.jsp
+++ web/getCommunity_service_council.jsp
@@ -1,17 +1,25 @@
-<%-- 
-    Document   : getCommunity_service_council
-    Created on : Jun 19, 2020, 12:08:36 AM
-    Author     : 20102
---%>
-
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-    </body>
-</html>
+<%@page import="model.Community_service_council"%>
+<%@page import="model.DB"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.sql.Connection"%>
+<%@page contentType="application/json" pageEncoding="UTF-8"%>
+<%
+    Connection con = DB.setConnection();
+    String output = "";
+    ArrayList<Community_service_council> b = DB.getCommunity_service_council(con, "select * from Community_service_council");
+    output += "{\"result\": [";
+    for (int i = 0; i < b.size(); i++) {
+        output += "{";
+        output += "\"csc_id\":\"" + b.get(i).getCsc_id() + "\",";
+        output += "\"csc_prof_id\":\"" + b.get(i).getCsc_prof_id() + "\",";
+        output += "\"csc_prof_state\":\"" + b.get(i).getCsc_prof_state() + "\",";
+        output += "\"csc_addedBy\":\"" + b.get(i).getCsc_addedBy() + "\",";
+        output += "\"name\":\"" + b.get(i).getName() + "\",";
+        output += "\"position\":\"" + b.get(i).getPosition() + "\",";
+        output += "\"image\":\"" + b.get(i).getBase64_image() + "\"";
+        output += "},";
+    }
+    output = output.substring(0, output.length() - 1);
+    output += "]}";
+%>
+<%=output%>
